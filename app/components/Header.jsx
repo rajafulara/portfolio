8c16cd11c8f63ed5707b8eb3b566631a4aaef0b8
Raja Fulara <rajafulara.rf@gmail.com>
2025-06-29 12:05:14 +0530

    Add priority attribute to Image components for improved loading performance

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,13 +11,13 @@ const Header = ({isDarkMode}) => {
         whileInView={{scale: 1}}
         transition={{duration: 0.8, type: 'spring', stiffness: 100}}
         >
-            <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+            <Image src={assets.profile_img} alt='' className='rounded-full w-32' priority={true}/>
         </motion.div>
         <motion.h3
         initial={{y: -20, opacity: 0}}
         whileInView={{y: 0, opacity: 1}}
         transition={{duration: 0.6, delay:0.3}}
-        className='flex items-end gap-2 text-xl md:text-2xl mb-3 '>Hi! I'm Raja Fulara <Image src={assets.hand_icon} alt='' className='w-6'/></motion.h3>
+        className='flex items-end gap-2 text-xl md:text-2xl mb-3 '>Hi! I'm Raja Fulara <Image src={assets.hand_icon} alt='' className='w-6' priority={true}/></motion.h3>
         <motion.h1
         initial={{y: -30, opacity: 0}}
         whileInView={{y: 0, opacity: 1}}
@@ -34,12 +34,12 @@ const Header = ({isDarkMode}) => {
             initial={{y: 30, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay:1}}
-            href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.a>
+            href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4' priority={true}/></motion.a>
             <motion.a
             initial={{y: 30, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay:1.2}}
-            href="https://www.linkedin.com/in/rajafulara/" target="_blank" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.right_arrow_bold} alt='' className='w-4'/></motion.a>
+            href="https://www.linkedin.com/in/rajafulara/" target="_blank" className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>my resume <Image src={assets.right_arrow_bold} alt='' className='w-4' priority={true}/></motion.a>
         </div>
     </div>
   )
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
-        <Image src={assets.header_bg_color} alt='' className='w-full'/>
+        <Image src={assets.header_bg_color} alt='' className='w-full' priority={true}/>
     </div>
         <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll?"bg-white/50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20":""}`}>
         <a href="#top" className="px-0 py-2.5 lg:px-10 lg:mr-[60px]">Portfolio</a>
@@ -43,11 +43,11 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             <div className='flex items-center gap-4 lg:px-10 lg:ml-[100px]'>
 
                 <button onClick={()=> setIsDarkMode(prev=> !prev)}>
-                    <Image src={isDarkMode?assets.sun_icon: assets.moon_icon} alt='' className='w-6'/>
+                    <Image src={isDarkMode?assets.sun_icon: assets.moon_icon} alt='' className='w-6' priority={true}/>
                 </button>
 
                 <button className='block md:hidden ml-3' onClick={openMenu}>
-                    <Image src={isDarkMode? assets.menu_white: assets.menu_black} alt='' className='w-6'/>
+                    <Image src={isDarkMode? assets.menu_white: assets.menu_black} alt='' className='w-6' priority={true}/>
                 </button>
             </div>
 
@@ -55,7 +55,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
 
                 <div className='absolute right-6 top-6' onClick={closeMenu}>
-                    <Image src={isDarkMode? assets.close_white: assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                    <Image src={isDarkMode? assets.close_white: assets.close_black} alt='' className='w-5 cursor-pointer' priority={true}/>
                 </div>
 
                 <li><a onClick={closeMenu} href="#top">Home</a></li>
