b9556e23d2d4d5c1b2bdaaa885ec96dd6e4a4424
Raja Fulara <121324875+rajafulara@users.noreply.github.com>
2025-07-13 15:08:39 +0530

    Update img alt Services.jsx

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -37,10 +37,10 @@ className="text-center mb-2 text-lg">What i offer</motion.h4>
                 <motion.div
                 whileHover={{scale: 1.05}}
                 key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
-                    <Image src={icon} alt='' className='w-10'/>
+                    <Image src={icon} alt={title} className='w-10'/>
                     <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                     <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>{description}</p>
-                    <a href={link} className='flex items-center gap-2 text-sm mt-5'>Show more <Image src={assets.right_arrow} alt='' className='w-4'/></a>
+                    <a href={link} className='flex items-center gap-2 text-sm mt-5'>Show more <Image src={assets.right_arrow} alt='right_arrow' className='w-4'/></a>
                 </motion.div>
             ))}
 
