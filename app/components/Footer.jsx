750b82e69e07097599fdba7527869783c620364c
Raja Fulara <121324875+rajafulara@users.noreply.github.com>
2025-07-15 14:00:40 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Footer = ({isDarkMode}) => {
   return (
     <div className='mt-20'>
-        <div className='text-center'>
+        {/* <div className='text-center'>
         <a href="#top" className="py-2.5">Raja Fulara</a>
         <div className='w-max flex items-center gap-2 mx-auto'>
         <Image src={isDarkMode? assets.mail_icon_dark: assets.mail_icon} alt='mail_icon' className='w-6'/>
-        rajafulara.rf@gmail.com
-        </div>
+        <a href="mailto:contact@rajafulara.me">contact@rajafulara.me</a>
+        </div> */}
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
           <p>© 2025 Raja Fulara. All rights reserved.</p>
           <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
