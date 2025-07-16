4511fea1d5cd79f63e455d446ea1bac85c4f632c
Raja Fulara <121324875+rajafulara@users.noreply.github.com>
2025-07-16 17:21:53 +0530

    Update person scema jsonld layout.js

--- app/layout.js
+++ app/layout.js
@@ -68,6 +68,15 @@ export const metadata = {
   },
 };
 
+const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Raja Fulara',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQHzaFj4Jio2UA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1727598419842?e=1758153600&v=beta&t=O2DC34o-_pbf1LQ5cS4abqDFsV3UzJKjmcjrm20cG34',
+    url: 'https://www.linkedin.com/in/rajafulara',
+  }
+ 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
@@ -76,24 +85,12 @@ export default function RootLayout({ children }) {
       >
         {children}
 
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org/",
-  "@type": "Person",
-  "name": "Raja Fulara",
-  "url": "https://rajafulara.me",
-  "image": "https://media.licdn.com/dms/image/v2/D5603AQHzaFj4Jio2UA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1727598419842?e=1758153600&v=beta&t=O2DC34o-_pbf1LQ5cS4abqDFsV3UzJKjmcjrm20cG34",
-  "sameAs": [
-    "https://www.linkedin.com/in/rajafulara",
-    "https://github.com/rajafulara"
-  ],
-  "jobTitle": "Professional",
-  "worksFor": {
-    "@type": "Organization",
-    "name": "Organization"
-  }  
-}
-</script>
+<script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c'),
+        }}
+      />
   
       </body>
     </html>
